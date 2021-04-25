06bc93cc0524617cc05eca430b25e738277a263c
Lexberg <martin.a.lexberg@hotmail.com>
2021-04-25 20:21:44 +0200

    Implement /api/profile and get access token

--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,15 @@ const express = require("express");
 const path = require("path")
 
 const app = express();
+app.get("/api/profile", (reg, res, next) => {
+    const authorization = reg.header("Authorization");
+    if(!authorization) {
+        return res.send(401);
+    }
+    return res.json({
+        username: "The master user",
+    });
+});
 app.use(express.static(path.resolve(__dirname, "..", "..", "dist")));
 app.use((reg, res, next) => {
     if (reg.method === "GET" && !reg.path.startsWith("/api")) {
