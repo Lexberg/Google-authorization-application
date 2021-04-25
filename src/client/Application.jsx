70f5bbab4b0baf9654a7a49666ba02df7270e171
Lexberg <martin.a.lexberg@hotmail.com>
2021-04-25 20:09:58 +0200

    ProfilePage Which GETs /api/profile

--- src/client/Application.jsx
+++ src/client/Application.jsx
@@ -1,8 +1,14 @@
 import * as React from "react";
 import {BrowserRouter, Link} from "react-router-dom";
 import {Route, Switch} from "react-router";
+import {ProfilePage} from "./ProfilePage";
+import {fetchJson} from "./http";
 
 export function Application(){
+    async function loadProfile() {
+        return fetchJson("/api/profile")
+    }
+
     return <BrowserRouter>
     <Switch>
         <Route path={"/"} exact>
@@ -15,6 +21,9 @@ export function Application(){
                     <Link to={"/login"}>Login</Link>
                 </li>
             </ul>
+        </Route>
+        <Route path={"/profile"}>
+            <ProfilePage loadProfile={loadProfile} />
         </Route>
         <Route  path={"/login"} exact>
             <h1>Login</h1>
